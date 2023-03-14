b6abfc7c8b853223413fb88f2d63cc3ce61cdfb6
gessnerda <gessnerda@gmail.com>
2023-03-14 13:28:12 -0600

    test

--- src/main/scala/edu/colorado/csci3155/project1/StackMachineCompiler.scala
+++ src/main/scala/edu/colorado/csci3155/project1/StackMachineCompiler.scala
@@ -6,49 +6,38 @@ object StackMachineCompiler {
 
     def compileToStackMachineCode(e: Expr): List[StackMachineInstruction] = {
         e match {
-            case Const(f) => List(PushNumI(f))
-
-            case Plus(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) ++ List(AddI)
-
-            case Minus(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) ++ List(SubI)
-
-            case Mult(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) ++ List(MultI)
-
-            case Div(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) ++ List(DivI)
-
-            case Exp(e1) => compileToStackMachineCode(e1) ++ List(ExpI)
-
-            case Log(e1) => compileToStackMachineCode(e1) ++ List(LogI)
-
-            case Sine(e1) => compileToStackMachineCode(e1) ++ List(SinI)
-
-            case Cosine(e1) => compileToStackMachineCode(e1) ++ List(CosI)
-
-            case Geq(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) ++ List(GeqI)
-
-            case Eq(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) ++ List(EqI)
-
-            case And(e1, e2) => compileToStackMachineCode(IfThenElse(e1, e2, Const(0)))
-
-            case Or(e1, e2) => compileToStackMachineCode(IfThenElse(e1, Const(1), e2))
-
-            case Not(e1) => compileToStackMachineCode(e1) ++ List(NotI)
-
-            case Let(s, e1, e2) =>
-                compileToStackMachineCode(e1) ++ List(PushEnv) ++ List(StoreEnv(s)) ++
-                compileToStackMachineCode(e2) ++ List(PopEnv)
-
-
-            case IfThenElse(e1, e2, e3) =>
-                val e1Instr = compileToStackMachineCode(e1)
-                val e2Instr = compileToStackMachineCode(e2)
-                val e3Instr = compileToStackMachineCode(e3)
-                e1Instr ++ List(CSkipI(e2Instr.length + 1)) ++ e2Instr ++ List(SkipI(e3Instr.length)) ++ e3Instr
-
-            case Ident(s) => List(LoadEnv(s))
-            
-
-            case _ => throw new IllegalArgumentException("Invalid expression")
+            case Const(c) => List(PushNumI(c))
+            case Plus(a, b) => compileToStackMachineCode(a) ++ compileToStackMachineCode(b) ++ List(AddI)
+            case Minus(a, b) => compileToStackMachineCode(a) ++ compileToStackMachineCode(b) ++ List(SubI)
+            case Mult(a, b) => compileToStackMachineCode(a) ++ compileToStackMachineCode(b) ++ List(MultI)
+            case Div(a, b) => compileToStackMachineCode(a) ++ compileToStackMachineCode(b) ++ List(DivI)
+            case Ident(id) => List(LoadEnv(id))
+            case Let(id, e1, e2) =>
+                compileToStackMachineCode(e1) ++
+                    List(LoadEnv(id), StoreEnv(id)) ++
+                    compileToStackMachineCode(e2) ++
+                    List(PopEnv)
+            case IfThenElse(cond, thenPart, elsePart) =>
+                val thenInstructions = compileToStackMachineCode(thenPart)
+                val elseInstructions = compileToStackMachineCode(elsePart)
+                val condInstructions = compileToStackMachineCode(cond)
+                condInstructions ++
+                    List(CSkipI(thenInstructions.length + 1)) ++
+                    thenInstructions ++
+                    List(SkipI(elseInstructions.length)) ++
+                    elseInstructions
+            case Geq(a, b) => compileToStackMachineCode(a) ++ compileToStackMachineCode(b) ++ List(GeqI)
+            case And(a, b) =>
+                val aInstructions = compileToStackMachineCode(a)
+                val bInstructions = compileToStackMachineCode(b)
+                aInstructions ++ List(CSkipI(bInstructions.length + 1)) ++ bInstructions ++ List(PushBoolI(false))
+            case Or(a, b) =>
+                val aInstructions = compileToStackMachineCode(a)
+                val bInstructions = compileToStackMachineCode(b)
+                aInstructions ++ List(CSkipI(2), PushBoolI(true), SkipI(bInstructions.length)) ++ bInstructions
+            case Not(a) => compileToStackMachineCode(a) ++ List(NotI)
+            case _ => throw new UnsupportedOperationException(s"Unsupported expression: $e")
         }
     }
 }
+
